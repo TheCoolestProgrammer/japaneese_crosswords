ee7d505c1f84d4d05868c3e26119918ac4adc2a8
mr_goldenman <vasya.chaukin.97@mail.ru>
2022-07-18 15:31:12 +0500

    trying to do windows

--- main.py
+++ main.py
@@ -7,17 +7,37 @@ clock = pygame.time.Clock()
 screen_width = 720
 screen_height = 720
 screen = pygame.display.set_mode((screen_width, screen_height))
-
+pygame.font.init()
+font = pygame.font.SysFont("Times New Roman",19)
+# font = pygame.font.SysFont("comicsansms", 40)
 fps = 60
 class Field:
     def __init__(self,menu_height,rows=10,columns=10):
         self.cell_size_x = screen_width//rows
         self.cell_size_y = (screen_height-menu_height)//columns
         self.field = [[0]*rows for y in range(columns)]
+
+class Window:
+    def __init__(self,text="window",width=300,height=200,x=200,y=200):
+        self.width = width
+        self.height = height
+        self.x = x
+        self.y = y
+        self.text = text
+        self.color = (128,128,128)
+        self.isactive = False
 class Menu:
-    def __init__(self):
-        self.height = 20
-        self.width = screen_width
+    def __init__(self,width,height,x,y,text=""):
+        self.height = height
+        self.width = width
+        self.x = x
+        self.y = y
+        self.color = (128,128,128)
+        self.submenu = []
+        self.link=[]
+        self.isactive = False
+        self.text = text
+
 def coordinates_changer_in_field(pos,field,menu):
     if pos[1]<menu.height or pos[0]<0:
         return None
@@ -33,7 +53,7 @@ def field_value_changer(value,field,menu):
     pos = coordinates_changer_in_field(pos, field, menu)
     if pos:
         field.field[pos[1]][pos[0]] = value
-def events_check(field,menu):
+def events_check(field,menu,menu_list):
     global process_running
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -43,6 +63,18 @@ def events_check(field,menu):
                 process_running = False
         elif event.type == pygame.MOUSEBUTTONDOWN:
             if event.button == 1:
+                pos = pygame.mouse.get_pos()
+                for item in menu_list:
+                    if len(item.submenu) == 0 and len(item.link)!=0:
+                        if item.x < pos[0] < item.x + item.width and item.y < pos[1] < item.y + item.height:
+                            item.link[0].isactive = True
+                            item.isactive = True
+                            break
+                    elif item.isactive:
+                        for j in item.submenu:
+                            if j.x< pos[0] < j.x+j.width and j.y< pos[1] < j.y+j.height:
+                                j.isactive = True
+                                break
                 field_value_changer(1,field,menu)
             if event.button == 3:
                 field_value_changer(0, field, menu)
@@ -52,8 +84,9 @@ def events_check(field,menu):
     elif keys[2]:
         field_value_changer(0, field, menu)
 
-def drawing(field,menu):
+def drawing(field,menu,menu_list):
     screen.fill((255, 255, 255))
+    #draw field
     for y in range(menu.height,screen_height,field.cell_size_y):
         pygame.draw.line(screen,(0,0,0),(0,y),(screen_width,y),3)
     for x in range(0,screen_width,field.cell_size_x):
@@ -64,16 +97,53 @@ def drawing(field,menu):
             if field.field[y][x] == 1:
                 pos = coordinates_changer_from_field((x,y),field,menu)
                 pygame.draw.rect(screen,(0,0,0),(pos[0],pos[1],field.cell_size_x,field.cell_size_y))
+    #draw menu
+    for item in menu_list:
+        if item.isactive:
+            pygame.draw.rect(screen,item.color,(item.x,item.y,item.width,item.height))
+            surface = font.render(item.text, False, (255, 255, 255))
+            screen.blit(surface, (item.x, item.y))
+            for j in item.submenu:
+                pygame.draw.rect(screen, j.color, (j.x, j.y, j.width, j.height))
+                surface = font.render(j.text,False,(255,255,255))
+                screen.blit(surface,(j.x,j.y))
+    #draw windows
+    for i in menu_list:
+        if i.isactive and len(i.link)>0 and i.link[0].isactive:
+            pygame.draw.rect(screen,(i.link[0].color),(i.link[0].x,i.link[0].y,i.link[0].width,i.link[0].height))
     pygame.display.update()
 
 def mainloop():
     global process_running
-    menu = Menu()
+    # describe menu
+    menu_width = 100
+    menu_height = 20
+    menu = Menu(screen_width,20,0,0)
+    #block 0
+    file_menu= Menu(menu_width,menu_height,0,0,"file")
+    field_menu= Menu(menu_width,menu_height,menu_width,0,"field")
+    menu.submenu.append(file_menu)
+    menu.submenu.append(field_menu)
+    menu.isactive = True
+    #block1
+    save_menu = Menu(menu_width,menu_height,0,menu_height,"save")
+    load_menu = Menu(menu_width,menu_height,0,menu_height*2,"load")
+    file_menu.submenu.append(save_menu)
+    file_menu.submenu.append(load_menu)
+
+    save_window = Window("save as")
+    load_window = Window("what kind load?")
+
+    save_menu.link.append(save_window)
+    load_menu.link.append(load_window)
+
+    menu_list = [menu,file_menu,save_menu,load_menu]
     field = Field(menu.height)
 
     while process_running:
-        events_check(field,menu)
-        drawing(field,menu)
+        events_check(field,menu,menu_list)
+        drawing(field,menu,menu_list)
+        print(file_menu.isactive)
         pygame.time.delay(fps)
 if __name__ == '__main__':
     mainloop()
